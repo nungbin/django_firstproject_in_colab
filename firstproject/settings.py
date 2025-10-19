0f01c5fed0247961610dc9963f4f5df981b42702
nungbin <shubinlin@gmail.com>
2025-10-19 00:11:20 +0000

    add postgresql to settings.py

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '???',
+        'USER': '???',
+        'PASSWORD': '???',
+        'HOST': 'localhost',
+        'PORT': '5432', # default PostgreSQL port
     }
 }
 
